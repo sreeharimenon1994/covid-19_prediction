ced4cd2634aa068207e3af5b473111baeb2065d0
Sreehari Menon <sm339@itl163.student.eecs.qmul.ac.uk>
2020-03-17 22:46:06 +0000

    api collection script

--- api.py
+++ api.py
@@ -1,7 +1,47 @@
+import pandas as pd
+import requests
+
 api_for_complete = 'https://services1.arcgis.com/0IrmI40n5ZYxTUrV/arcgis/rest/services/DailyConfirmedCases/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=DateVal%20asc&outSR=102100&resultOffset=0&resultRecordCount=2000&cacheHint=true'
-
-
 apiCityDay = 'https://services1.arcgis.com/0IrmI40n5ZYxTUrV/arcgis/rest/services/CountyUAs_cases/FeatureServer/0/query?f=json&where=TotalCases%20%3C%3E%200&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=TotalCases%20desc&outSR=102100&resultOffset=0&resultRecordCount=1000&cacheHint=true'
-
-
 overallCases = 'https://services1.arcgis.com/0IrmI40n5ZYxTUrV/arcgis/rest/services/DailyIndicators/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&outSR=102100&resultOffset=0&resultRecordCount=50&cacheHint=true'
+
+
+#overall data per day
+data = requests.get(overallCases).json()['features'][0]['attributes']
+df = list(data.items())
+vals = [x[1] for x in df]
+cols = [x[0] for x in df]
+
+df = pd.DataFrame([vals], columns=cols)
+csvname = 'data/overall.csv'
+dat = pd.read_csv(csvname)
+dat = dat.append(df)
+dat.to_csv(csvname, index=False)
+
+
+#cases per day per city
+data = requests.get(apiCityDay).json()['features']
+vals = []
+cols = []
+for x in data:
+    x = x['attributes']
+    vals.append(x['TotalCases'])
+    cols.append(x['GSS_NM'])
+
+
+df = pd.DataFrame([vals], columns=cols)
+csvname = 'data/casescityday.csv'
+dat = pd.read_csv(csvname)
+dat = dat.append(df)
+dat.to_csv(csvname, index=False)
+
+
+#from graphy
+data = requests.get(api_for_complete).json()['features']
+arr = []
+for x in data:
+    x = x['attributes']
+    arr.append([x['DateVal'], x['CMODateCount']])
+
+with open("data/graph.txt", "w") as graph:
+    graph.write(str(arr))
